db70b3de6d24609dbbc22d66018317e87329889c
Michael F. Booth <mike@mechanicalrobotfish.com>
2014-04-18 14:51:20 -0400

    Work around bug in Virtualbox Guest Additions 4.3.10.

--- precise64/scripts/guest_additions.sh
+++ precise64/scripts/guest_additions.sh
@@ -14,4 +14,8 @@ umount /mnt/vg
 rmdir /mnt/vg
 rm /home/vagrant/VBoxGuestAdditions*.iso
 
-
+# Emergency symlink to fix https://www.virtualbox.org/ticket/12879
+# We can remove this after Virtualbox 4.3.11 is released.
+if [ -d /opt/VBoxGuestAdditions-4.3.10 ]; then
+    ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+fi
